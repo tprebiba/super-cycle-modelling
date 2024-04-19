639d09ae3144f8c95fc7e2c5b7fca062991a94b3
tprebiba <tirsi.prebibaj@cern.ch>
2024-04-19 13:26:14 +0200

    refined supercycles_scenario_to_dataframe

--- helpers.py
+++ helpers.py
@@ -13,7 +13,7 @@ def load_cycle_from_csv(filepath,xkey='logical.MBI/IMAINS',ykey='logical.MBI/IMA
     return df[xkey], df[ykey]
 
 
-def cycles_to_dataframe(cycles):
+def cycles_to_dataframe(cycles, write_to_latextable=False):
     # Gather SPS cycles in a dataframe
     df_cycles = pnd.DataFrame(columns=['Length [s]', 'BPs', 'Power (MB+MQ) [MW]']) 
     for c in cycles.keys():
@@ -21,50 +21,42 @@ def cycles_to_dataframe(cycles):
                                                 data={'Length [s]': '%1.1f'%(cycles[c].length), 
                                                       'BPs': '%1.1f'%(cycles[c].bps),
                                                       'Power (MB+MQ) [MW]': '%1.2f'%(cycles[c].power)}))
-    #df_to_latextable(df_cycles, filename='latex_tables/cycles.tex', column_format='lcc')
+    if write_to_latextable:
+        print('Writing cycles to latex table.')
+        df_to_latextable(df_cycles, filename='latex_tables/cycles.tex', column_format='lcc')
     return df_cycles
 
 
-def supercycles_scenario_to_dataframe(supercycles_scenario, 
-                                     save_to_latex_file=False):
+def supercycles_scenario_to_dataframe(supercycles_scenario, write_to_latextable=False):
 
     # Get all cycles from supercycles scenario
-    cycles_list = []
+    cycle_names = []
     for supercycle in supercycles_scenario.keys():
-        cycles_list += supercycles_scenario[supercycle].cycle_names
-    cycle_list = list(np.unique(cycles_list))
-
-    df = pnd.DataFrame(columns=cycle_list+['Length [s]', 'Power [MW]']) 
+        cycle_names += supercycles_scenario[supercycle].cycle_names
+    cycle_names = list(np.unique(cycle_names))
+    df = pnd.DataFrame(columns=cycle_names+['Length [s]', 'Power [MW]']) 
     
     for supercycle in supercycles_scenario.keys():
         data = {}
-        for cycle in cycle_list:
-            if cycle in supercycles_scenario[supercycle].cycle_names:
-                data[cycle] = '1'
-            else:
-                data[cycle] = '-'
+        
         data['Length [s]'] = supercycles_scenario[supercycle].length
         data['Power [MW]'] = round(supercycles_scenario[supercycle].average_power,2)
+
+        for cycle in cycle_names:
+            data[cycle] = int(0)
+
+        for cycle in supercycles_scenario[supercycle].cycle_names:
+                data[cycle] += int(1)
+        
+        for cycle in cycle_names:
+            if data[cycle] == 0:
+                data[cycle] = '-'
+            elif data[cycle] > 0:
+                data[cycle] = str(int(data[cycle]))
+        
         df = df._append(pnd.Series(name=supercycle,data=data))
 
-    if save_to_latex_file:
+    if write_to_latextable:
         df_to_latextable(df, filename='latex_tables/shared_TCC8.tex', column_format='l|ccccccccccc|cc')
 
     return df
-
-
-def schedule_to_dataframe(schedule,total_hours, total_hours_recalculated, fraction_no_LHC, fraction):
-    df_schedule = pnd.DataFrame(columns=['Scheduled [hours]', 'Effective [hours]', 'Super-cycle percentage [%]']) 
-    for c in schedule.keys():
-        data = {'Scheduled [hours]': '%1.1f'%(schedule[c]/fraction_no_LHC), 
-                'Effective [hours]': '%1.2f'%(schedule[c]),
-                'Super-cycle percentage [%]': '%1.2f'%(schedule[c]/total_hours*100)}
-        name = c
-        if 'LHC' in c:
-            data = {'Scheduled [hours]': '-', 
-                    'Effective [hours]': '%1.2f'%(schedule[c]),
-                    'Super-cycle percentage [%]': '%1.2f'%(schedule[c]/total_hours*100)}
-            name = c + ' (%i%% of time)'%(fraction[c]*100)
-        df_schedule = df_schedule.append(pnd.Series(name=name,data=data))
-    df_schedule = df_schedule.append(pnd.Series(name='Total',data={'Scheduled [hours]': total_hours, 'Effective [hours]': total_hours_recalculated, 'Super-cycle percentage [%]': total_hours/total_hours_recalculated*100}))
-    return df_schedule
